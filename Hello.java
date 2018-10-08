40ebf8a3cbd54a711dc1a54e187a45a8bdcfa073
Papulov Oleg <defaultt96@mail.ru>
2018-10-08 13:35:30 +0300

    commit status

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
 	System.out.println("a");
-        System.out.println("Hello World!");
+        System.out.println("Hello World!1");
 	System.out.println("b");
     }
 }
