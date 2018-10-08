afd3e19c656d5453c309bd0e9d51a9220b36ac46
Papulov Oleg <defaultt96@mail.ru>
2018-10-08 13:39:50 +0300

    commit cd h

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
 	System.out.println("a");
-        System.out.println("Hello World!");
+        System.out.println("Hello World!2");
 	System.out.println("b");
     }
 }
